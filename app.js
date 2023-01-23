e2c0df38f3a3d834c0fb38cca49923d24f08b272
EstebanIslas <1717110261@utectulancingo.edu.mx>
2023-01-23 11:19:00 -0600

    Obteniendo datos de la app y enviarlos a una clase

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Product {
-    construct(name, price, year){
+    constructor(name, price, year){
         this.name = name;
         this.price = price;
         this.year = year;
@@ -22,7 +22,17 @@ class UI {
 }
 
 //DOM Events
-document.getElementById('product-form').addEventListener('submit', function() { //Obteniendo datos del form
+document.getElementById('product-form')
+    .addEventListener('submit', function(e) { //Obteniendo datos del form
+        const name = document.getElementById('name').value;
+        const price = document.getElementById('price').value;
+        const year = document.getElementById('year').value;
+        
+        //Definir productos para crear nuevo objeto
+        const product = new Product(name, price, year);
 
+        
 
+        //Cancelar evento reload de la app
+        e.preventDefault();
 });
