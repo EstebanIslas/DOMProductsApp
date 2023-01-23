a25fd3cb6977b48780186ff4742d23fed0a030fc
EstebanIslas <1717110261@utectulancingo.edu.mx>
2023-01-23 11:05:15 -0600

    Creacion de Clases, Metodos y datos DOM

---
+++ app.js
@@ -0,0 +1,28 @@
+class Product {
+    construct(name, price, year){
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+class UI { 
+    //Metodos dentro de la Interface
+    addProduct(){
+
+    }
+
+    deleteProduct(){
+
+    }
+
+    showMessage(){
+
+    }
+}
+
+//DOM Events
+document.getElementById('product-form').addEventListener('submit', function() { //Obteniendo datos del form
+
+
+});
